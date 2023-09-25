632dd21365f013f7e3421712b87498114d1f3c6c
Roar Mjelde <roar.mjelde@digdir.no>
2023-09-25 16:03:05 +0200

    Updated blob test to use Options pattern

--- Test/Altinn.Broker.Tests/Persistence/BlobStoreTests.cs
+++ Test/Altinn.Broker.Tests/Persistence/BlobStoreTests.cs
@@ -1,6 +1,8 @@
 using Altinn.Broker.Persistence;
+using Altinn.Broker.Persistence.Options;
 
 using Azure.Storage.Blobs;
+using Microsoft.Extensions.Options;
 
 public class BlobStoreTests
 {
@@ -8,8 +10,10 @@ public class BlobStoreTests
     public async void StoreBlob_HappyPath_BlobIsStored()
     {
         // Arrange
-        Environment.SetEnvironmentVariable("BlobStorageConnectionString", "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;");
-        var blobStore = new BlobStore();
+        IOptions<StorageOptions> options = Options.Create(new StorageOptions(){
+            ConnectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
+        });
+        var blobStore = new BlobStore(options);
 
         // Act
         FileStream fileStream = File.Open("../../../Persistence/data/test.txt", FileMode.Open);
