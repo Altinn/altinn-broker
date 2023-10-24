8723fc18a0a4b3a2a512a8d34a554188ce013c43
tsh <acn-thage@ai-dev.no>
2023-10-24 11:06:08 +0200

    oppdatert, avventer decision på disse

--- src/Altinn.Broker.Core/Domain/File.cs
+++ src/Altinn.Broker.Core/Domain/File.cs
@@ -5,11 +5,11 @@ namespace Altinn.Broker.Core.Domain;
 public class File
 {
     public Guid FileId { get; set; }
-    public string ExternalFileReference { get; set; } = string.Empty;
+    public string ExternalFileReference { get; set; };
     public Guid ShipmentId { get; set; }
     public FileStatus FileStatus { get; set; } // Joined in
     public DateTimeOffset? LastStatusUpdate { get; set; }
     public DateTimeOffset Uploaded { get; set; }
-    public string FileLocation { get; set; } = string.Empty; // Joined in
+    public string FileLocation { get; set; }; // Joined in
     public List<FileReceipt> Receipts { get; set; } // Joined in
 }
