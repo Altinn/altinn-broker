0863aa607a136c8d3a775a499025cf3f7d6f7e2e
Roar Mjelde <roarmj@gmail.com>
2023-11-15 10:42:58 +0100

    Updated blob store classes to accomodate custom connection string, wip

--- src/Altinn.Broker.Persistence/BlobStore.cs
+++ src/Altinn.Broker.Persistence/BlobStore.cs
@@ -2,33 +2,53 @@ using Altinn.Broker.Persistence.Options;
 
 using Azure.Storage.Blobs;
 
+using Microsoft.Azure.Management.Storage;
 using Microsoft.Extensions.Options;
 
 namespace Altinn.Broker.Persistence;
 
 public class BlobStore : IFileStore
 {
-    private static string? _connectionString;
+    private static string _connectionString;
 
     public BlobStore(IOptions<StorageOptions> storageOptions)
     {
         _connectionString = storageOptions.Value.ConnectionString ?? throw new ArgumentNullException("StorageOptions__ConnectionString");
     }
 
-    public async Task<Stream> GetFileStream(Guid fileId)
+    public async Task<Stream> GetFileStream(Guid fileId, string? connectionString)
     {
-        var containerClient = new BlobContainerClient(_connectionString, "files");
+        var containerClient = new BlobContainerClient(connectionString ?? _connectionString, "files");
         await containerClient.CreateIfNotExistsAsync();
         BlobClient blobClient = containerClient.GetBlobClient(fileId.ToString());
         var content = await blobClient.DownloadContentAsync();
         return content.Value.Content.ToStream();
     }
 
-    public async Task UploadFile(Stream stream, Guid fileId)
+    public async Task UploadFile(Stream stream, Guid fileId, string? connectionString)
     {
-        var containerClient = new BlobContainerClient(_connectionString, "files");
+        var containerClient = new BlobContainerClient(connectionString ?? _connectionString, "files");
         await containerClient.CreateIfNotExistsAsync();
         BlobClient blobClient = containerClient.GetBlobClient(fileId.ToString());
         await blobClient.UploadAsync(stream, true);
+    }
+
+    public async Task<bool> IsOnline(string? connectionString) 
+    {
+        try
+        {
+            var blobServiceClient = new BlobServiceClient(connectionString ?? _connectionString);
+            var containers = blobServiceClient.GetBlobContainers();
+            foreach (var container in containers)
+            {
+                // Accessing the containers. If this succeeds, the account exists.
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
     }
 }
--- src/Altinn.Broker.Persistence/Repositories/IFileStore.cs
+++ src/Altinn.Broker.Persistence/Repositories/IFileStore.cs
@@ -2,7 +2,8 @@ namespace Altinn.Broker.Persistence
 {
     public interface IFileStore
     {
-        Task<Stream> GetFileStream(Guid fileId);
-        Task UploadFile(Stream filestream, Guid fileId);
+        Task<Stream> GetFileStream(Guid fileId, string? connectionString);
+        Task UploadFile(Stream filestream, Guid fileId, string? connectionString);
+        Task<bool> IsOnline(string? connectionString);
     }
 }
