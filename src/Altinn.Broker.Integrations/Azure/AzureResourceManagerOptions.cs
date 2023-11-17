44decfbaa964367af1bdf87b72d46498dd79e09f
Roar Mjelde <roarmj@gmail.com>
2023-11-17 16:56:15 +0100

    Updated options to work with resource deployment

---
+++ src/Altinn.Broker.Integrations/Azure/AzureResourceManagerOptions.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Core;
+
+namespace Altinn.Broker.Integrations.Azure;
+
+public class AzureResourceManagerOptions
+{
+    public ResourceIdentifier SubscriptionId { get; internal set; }
+    public AzureLocation Location { get; internal set; }
+    public string ClientSecret { get; internal set; }
+    public string ClientId { get; internal set; }
+    public string TenantId { get; internal set; }
+}
