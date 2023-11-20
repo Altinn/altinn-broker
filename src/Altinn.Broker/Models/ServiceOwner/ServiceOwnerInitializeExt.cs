c61ae16ff6b8e206e856f7d90e363278cbb165ab
Roar Mjelde <roarmj@gmail.com>
2023-11-20 12:57:24 +0100

    Changed to use DataAnnotations because FluentValidation no longer intends auto-validation to be in their feature set: https://github.com/FluentValidation/FluentValidation.AspNetCore

--- src/Altinn.Broker/Models/ServiceOwner/ServiceOwnerInitializeExt.cs
+++ src/Altinn.Broker/Models/ServiceOwner/ServiceOwnerInitializeExt.cs
@@ -1,4 +1,6 @@
-﻿namespace Altinn.Broker.Models.ServiceOwner;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Altinn.Broker.Models.ServiceOwner;
 
 public class ServiceOwnerInitializeExt
 {
@@ -7,6 +9,7 @@ public class ServiceOwnerInitializeExt
     /// <summary>
     /// This should be on the form countrycode:organizationnumber. For instance 0192:922444555 for a Norwegian organization with org number 923 444 555. Corresponds to consumer.id in Maskinporten token.
     /// </summary>
+    [RegularExpressionAttribute(@"^\d{4}:\d{9}$", ErrorMessage = "ServiceOwnerId should be on the Maskinporten form with countrycode:organizationnumber, for instance 0192:910753614")]
     public string Id { get; set; }
 
     public string Name { get; set; }
