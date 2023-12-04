609c8a7cb0bc99098691d86fc8dd86b938ae6d72
Roar Mjelde <roarmj@gmail.com>
2023-12-04 14:58:50 +0100

    Fix tests

--- Test/Altinn.Broker.Tests/FileControllerTests.cs
+++ Test/Altinn.Broker.Tests/FileControllerTests.cs
@@ -6,23 +6,35 @@ using System.Text.Json;
 using Altinn.Broker.Core.Models;
 using Altinn.Broker.Enums;
 using Altinn.Broker.Models;
+using Altinn.Broker.Tests.Helpers;
 
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Identity.Web;
 
 using Xunit;
 
 namespace Altinn.Broker.Tests;
-public class FileControllerTests : IClassFixture<WebApplicationFactory<Program>>
+public class FileControllerTests : IClassFixture<CustomWebApplicationFactory>
 {
     private readonly WebApplicationFactory<Program> _factory;
-    private readonly HttpClient _client;
+    private readonly HttpClient _senderClient;
+    private readonly HttpClient _recipientClient;
     private readonly JsonSerializerOptions _responseSerializerOptions;
 
-    public FileControllerTests(WebApplicationFactory<Program> factory)
+    /**
+     * Inject a mock bearer configuration that does not verify anything. 
+     * Generate our own JWT with correct scope, expiry and issuer. 
+     * Set it as default request header
+     * */
+
+    public FileControllerTests(CustomWebApplicationFactory factory)
     {
         _factory = factory;
-        _client = factory.CreateClient();
-        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwMTkyOjk5MTgyNTgyNyJ9.exFSD-mL1fzoWhg8IKcVeCeEyJ5qpABPU9A1AXHDa_k");
+        _senderClient = factory.CreateClient();
+        _senderClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", TestConstants.DUMMY_SENDER_TOKEN);
+        _recipientClient = factory.CreateClient();
+        _recipientClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", TestConstants.DUMMY_RECIPIENT_TOKEN);
+        
         _responseSerializerOptions = new JsonSerializerOptions(new JsonSerializerOptions()
         {
             PropertyNameCaseInsensitive = true
@@ -34,18 +46,18 @@ public class FileControllerTests : IClassFixture<WebApplicationFactory<Program>>
     [Fact]
     public async Task WhenAllIsOk_NormalFlow_Success()
     {
-        var initializeFileResponse = await _client.PostAsJsonAsync("broker/api/v1/file", new FileInitalizeExt()
+        var initializeFileResponse = await _senderClient.PostAsJsonAsync("broker/api/v1/file", new FileInitalizeExt()
         {
             Checksum = null,
             FileName = "input.txt",
             PropertyList = [],
-            Recipients = new List<string> { "974761076" },
-            Sender = "991825827",
+            Recipients = new List<string> { "0192:986252932" },
+            Sender = "0192:991825827",
             SendersFileReference = "test-data"
         });
         var fileId = await initializeFileResponse.Content.ReadAsStringAsync();
 
-        var initializedFile = await _client.GetFromJsonAsync<FileOverviewExt>($"broker/api/v1/file/{fileId}", _responseSerializerOptions);
+        var initializedFile = await _senderClient.GetFromJsonAsync<FileOverviewExt>($"broker/api/v1/file/{fileId}", _responseSerializerOptions);
         Assert.NotNull(initializedFile);
         Assert.True(initializedFile.FileStatus == FileStatusExt.Initialized);
 
@@ -53,26 +65,26 @@ public class FileControllerTests : IClassFixture<WebApplicationFactory<Program>>
         using (var content = new ByteArrayContent(uploadedFileBytes))
         {
             content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-            var uploadResponse = await _client.PostAsync($"broker/api/v1/file/{fileId}/upload", content);
+            var uploadResponse = await _senderClient.PostAsync($"broker/api/v1/file/{fileId}/upload", content);
             Assert.True(uploadResponse.IsSuccessStatusCode);
         }
 
-        var uploadedFile = await _client.GetFromJsonAsync<FileOverviewExt>($"broker/api/v1/file/{fileId}", _responseSerializerOptions);
+        var uploadedFile = await _senderClient.GetFromJsonAsync<FileOverviewExt>($"broker/api/v1/file/{fileId}", _responseSerializerOptions);
         Assert.NotNull(uploadedFile);
         Assert.True(uploadedFile.FileStatus == FileStatusExt.Published); // When running integration test this happens instantly as of now.
 
-        var downloadedFile = await _client.GetAsync($"broker/api/v1/file/{fileId}/download");
+        var downloadedFile = await _recipientClient.GetAsync($"broker/api/v1/file/{fileId}/download");
         var downloadedFileBytes = await downloadedFile.Content.ReadAsByteArrayAsync();
         Assert.Equal(uploadedFileBytes, downloadedFileBytes);
 
-        var downloadedFileDetails = await _client.GetFromJsonAsync<FileStatusDetailsExt>($"broker/api/v1/file/{fileId}/details", _responseSerializerOptions);
+        var downloadedFileDetails = await _senderClient.GetFromJsonAsync<FileStatusDetailsExt>($"broker/api/v1/file/{fileId}/details", _responseSerializerOptions);
         Assert.NotNull(downloadedFileDetails);
         Assert.True(downloadedFileDetails.FileStatus == FileStatusExt.Published);
         Assert.Contains(downloadedFileDetails.RecipientFileStatusHistory, recipient => recipient.RecipientFileStatusCode == RecipientFileStatusExt.DownloadStarted);
 
-        await _client.PostAsync($"broker/api/v1/file/{fileId}/confirmdownload", null);
+        await _recipientClient.PostAsync($"broker/api/v1/file/{fileId}/confirmdownload", null);
 
-        var confirmedFileDetails = await _client.GetFromJsonAsync<FileStatusDetailsExt>($"broker/api/v1/file/{fileId}/details", _responseSerializerOptions);
+        var confirmedFileDetails = await _senderClient.GetFromJsonAsync<FileStatusDetailsExt>($"broker/api/v1/file/{fileId}/details", _responseSerializerOptions);
         Assert.NotNull(confirmedFileDetails);
         Assert.True(confirmedFileDetails.FileStatus == FileStatusExt.AllConfirmedDownloaded);
         Assert.Contains(confirmedFileDetails.RecipientFileStatusHistory, recipient => recipient.RecipientFileStatusCode == RecipientFileStatusExt.DownloadConfirmed);
---
+++ Test/Altinn.Broker.Tests/Helpers/CustomWebApplicationFactory.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(
+        IWebHostBuilder builder)
+    {
+        // Overwrite registrations from Program.cs
+        builder.ConfigureTestServices((services) =>
+        {
+            var authenticationBuilder = services.AddAuthentication();
+            authenticationBuilder.Services.Configure<AuthenticationOptions>(o =>
+            {
+                o.SchemeMap.Clear();
+                ((IList<AuthenticationSchemeBuilder>)o.Schemes).Clear();
+            });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(async options =>
+            {                
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = false,
+                    RequireExpirationTime = false,
+                    RequireSignedTokens = false,
+                    SignatureValidator = delegate (string token, TokenValidationParameters parameters)
+                    {
+                        var jwt = new JsonWebToken(token);
+                        return jwt;
+                    }
+                };
+            });
+        });
+    }
+}
