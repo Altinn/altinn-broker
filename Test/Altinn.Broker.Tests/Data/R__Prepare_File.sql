284bf132451e0bddad0647fefdd59ef72c381b5b
Roar Mjelde <roarmj@gmail.com>
2023-12-12 14:44:13 +0100

    Try with joining the repeatable migration sripts together. Seems one fails to run if it is dependent on the other one when in a brand new environment. Should find better way to split up in more test scripts if we get big script.

--- Test/Altinn.Broker.Tests/Data/R__Prepare_File.sql
+++
@@ -1,4 +0,0 @@
-
-
-INSERT INTO broker.file (file_id_pk, service_owner_id_fk, filename, checksum, sender_actor_id_fk, external_file_reference, created, storage_provider_id_fk, file_location, expiration_time)
-VALUES ('ed76ce89-3768-481a-bca1-4e4262d9098b', '0192:991825827', 'filename.txt', NULL, 1, 'external_reference', NOW(), 1, 'https://blob-storage-example', NOW() + INTERVAL '1 DAY')
