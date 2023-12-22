23772f610eddafd5d15944361356133aa5b4c696
Roar Mjelde <roarmj@gmail.com>
2023-12-22 08:52:17 +0100

    Added logging of 400 errors

---
+++ src/Altinn.Broker/Helpers/ValidationErrorLogger.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.Broker.Helpers;
+
+public static class ValidationErrorLogger
+{
+    public static void LogValidationError(ActionContext context)
+    {
+        var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+        foreach (var entry in context.ModelState)
+        {
+            foreach (var error in entry.Value.Errors)
+            {
+                logger.LogWarning("Validation error: " + error.ErrorMessage);
+            }
+        }
+    }
+}
