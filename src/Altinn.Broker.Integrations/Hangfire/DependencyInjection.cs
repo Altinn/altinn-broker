51f462f00587762552d6caf1135aaccbaad36d90
Roar Mjelde <36594318+Ceredron@users.noreply.github.com>
2024-01-02 16:27:01 +0100

    Made dashboard separate application (#243)

--- src/Altinn.Broker.Integrations/Hangfire/DependencyInjection.cs
+++ src/Altinn.Broker.Integrations/Hangfire/DependencyInjection.cs
@@ -3,31 +3,11 @@
 using Hangfire;
 using Hangfire.PostgreSql;
 
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace Altinn.Broker.Integrations.Hangfire;
 public static class DependencyInjection
 {
-    public static void ConfigureHangfireDashboard(this WebApplication app, IConfiguration config)
-    {
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseHangfireDashboard();
-        }
-        else
-        {
-            var hangfireAuthOptions = new HangfireAuthorizationOptions();
-            config.GetSection(nameof(HangfireAuthorizationOptions)).Bind(hangfireAuthOptions);
-            app.UseHangfireDashboard("/hangfire", new DashboardOptions
-            {
-                Authorization = new[] { new HangfireMaintainerAuthorizationFilter(hangfireAuthOptions) }
-            });
-        }
-    }
-
     public static void ConfigureHangfire(this IServiceCollection services)
     {
         var serviceProvider = services.BuildServiceProvider();
