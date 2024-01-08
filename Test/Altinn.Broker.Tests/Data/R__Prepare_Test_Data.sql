2a8af74886b2f764820cc76e5060ee66bc5d7ec1
Roar Mjelde <roarmj@gmail.com>
2024-01-08 16:14:50 +0100

    Updated tests to new Service model

--- Test/Altinn.Broker.Tests/Data/R__Prepare_Test_Data.sql
+++ Test/Altinn.Broker.Tests/Data/R__Prepare_Test_Data.sql
@@ -4,5 +4,8 @@ VALUES ('0192:991825827', 'Digitaliseringsdirektoratet Avd Oslo', '1 Days');
 INSERT INTO broker.storage_provider (storage_provider_id_pk, service_owner_id_fk, created, storage_provider_type, resource_name)
 VALUES (DEFAULT, '0192:991825827', NOW(), 'Altinn3Azure', 'dummy-value');
 
-INSERT INTO broker.file (file_id_pk, service_owner_id_fk, filename, checksum, sender_actor_id_fk, external_file_reference, created, storage_provider_id_fk, file_location, expiration_time)
-VALUES ('ed76ce89-3768-481a-bca1-4e4262d9098b', '0192:991825827', 'filename.txt', NULL, 1, 'external_reference', NOW(), 1, 'https://blob-storage-example', NOW() + INTERVAL '1 Days');
+INSERT INTO broker.service (service_id_pk, created, client_id, organization_number, service_owner_id_fk)
+VALUES (DEFAULT, NOW(), '00000000-0000-0000-0000-000000000000', '0192:991825827', '0192:991825827');
+
+INSERT INTO broker.service (service_id_pk, created, client_id, organization_number, service_owner_id_fk)
+VALUES (DEFAULT, NOW(), '11111111-1111-1111-1111-111111111111', '0192:991825832', '0192:991825827');
