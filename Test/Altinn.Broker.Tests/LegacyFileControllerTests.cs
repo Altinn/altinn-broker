3587667f42280731e31873bb7572b94fe36ffbf7
Ragnar Inge Fatland <acn-rfatl@ai-dev.no>
2024-01-11 10:39:39 +0100

    Initial commit for LegacyFileController that will be used to provide Broker Service to the legacy Altinn 2 implementation. Minor changes in this version, will be changed further as implementation of specific methods are performed.

---
+++ Test/Altinn.Broker.Tests/LegacyFileControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Specialized;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+using Altinn.Broker.Core.Models;
+using Altinn.Broker.Enums;
+using Altinn.Broker.Models;
+using Altinn.Broker.Tests.Factories;
+using Altinn.Broker.Tests.Helpers;
+
+using Hangfire.Common;
+using Hangfire.States;
+
+using Moq;
+
+using Xunit;
+
+namespace Altinn.Broker.Tests;
+public class LegacyFileControllerTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly CustomWebApplicationFactory _factory;
+    private readonly HttpClient _senderClient;
+    private readonly HttpClient _recipientClient;
+    private readonly JsonSerializerOptions _responseSerializerOptions;
+
+    public LegacyFileControllerTests(CustomWebApplicationFactory factory)
+    {
+        _factory = factory;
+        _senderClient = factory.CreateClient();
+        _senderClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", TestConstants.DUMMY_SENDER_TOKEN);
+        _recipientClient = factory.CreateClient();
+        _recipientClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", TestConstants.DUMMY_RECIPIENT_TOKEN);
+
+        _responseSerializerOptions = new JsonSerializerOptions(new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        });
+        _responseSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    }
+
+}
---
+++ src/Altinn.Broker/Controllers/LegacyFileController.cs
@@ -0,0 +1,217 @@
+using Altinn.Broker.Application.ConfirmDownloadCommand;
+using Altinn.Broker.Application.DownloadFileQuery;
+using Altinn.Broker.Application.GetFileDetailsQuery;
+using Altinn.Broker.Application.GetFileOverviewQuery;
+using Altinn.Broker.Application.GetFilesQuery;
+using Altinn.Broker.Application.InitializeFileCommand;
+using Altinn.Broker.Application.UploadFileCommand;
+using Altinn.Broker.Core.Domain.Enums;
+using Altinn.Broker.Core.Models;
+using Altinn.Broker.Enums;
+using Altinn.Broker.Helpers;
+using Altinn.Broker.Mappers;
+using Altinn.Broker.Middlewares;
+using Altinn.Broker.Models;
+using Altinn.Broker.Models.Maskinporten;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Altinn.Broker.Controllers
+{
+    /// <summary>
+    /// The LegacyFileController allows integration from the Altinn 2 BrokerBridge component to allow legacy users access to Altinn 3 Broker
+    /// </summary>
+    [ApiController]
+    [Route("broker/api/legacy/v1/file")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize(Policy = "Legacy")]
+    public class LegacyFileController : Controller
+    {
+        private readonly ILogger<LegacyFileController> _logger;
+        private readonly ProblemDetailsFactory _problemDetailsFactory;
+
+        public LegacyFileController(ILogger<LegacyFileController> logger, ProblemDetailsFactory problemDetailsFactory)
+        {
+            _logger = logger;
+            _problemDetailsFactory = problemDetailsFactory;
+        }
+
+        /// <summary>
+        /// Initialize a file upload
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]        
+        public async Task<ActionResult<Guid>> InitializeFile(FileInitalizeExt initializeExt, [ModelBinder(typeof(MaskinportenModelBinder))] MaskinportenToken token, [FromServices] InitializeFileCommandHandler handler)
+        {
+            LogContextHelpers.EnrichLogsWithInitializeFile(initializeExt);
+            LogContextHelpers.EnrichLogsWithMaskinporten(token);
+            _logger.LogInformation("Initializing file");
+            var commandRequest = InitializeFileMapper.MapToRequest(initializeExt, token);
+            var commandResult = await handler.Process(commandRequest);
+            return commandResult.Match(
+                fileId => Ok(fileId.ToString()),
+                error => Problem(detail: error.Message, statusCode: (int)error.StatusCode)
+            );
+        }
+
+        /// <summary>
+        /// Upload to an initialized file using a binary stream.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{fileId}/upload")]
+        [Consumes("application/octet-stream")]
+        public async Task<ActionResult> UploadFileStreamed(
+            Guid fileId,
+            [ModelBinder(typeof(MaskinportenModelBinder))] MaskinportenToken token,
+            [FromServices] UploadFileCommandHandler handler
+        )
+        {
+            LogContextHelpers.EnrichLogsWithMaskinporten(token);
+            _logger.LogInformation("Uploading file {fileId}", fileId.ToString());
+            Request.EnableBuffering();
+            var commandResult = await handler.Process(new UploadFileCommandRequest()
+            {
+                FileId = fileId,
+                Consumer = token.Consumer,
+                Supplier = token.Supplier,
+                Filestream = Request.Body
+            });
+            return commandResult.Match(
+                fileId => Ok(fileId.ToString()),
+                error => Problem(detail: error.Message, statusCode: (int)error.StatusCode)
+            );
+        }
+
+        /// <summary>
+        /// Get information about the file and its current status
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{fileId}")]
+        public async Task<ActionResult<FileOverviewExt>> GetFileOverview(
+            Guid fileId,
+            [ModelBinder(typeof(MaskinportenModelBinder))] MaskinportenToken token,
+            [FromServices] GetFileOverviewQueryHandler handler)
+        {
+            LogContextHelpers.EnrichLogsWithMaskinporten(token);
+            _logger.LogInformation("Getting file overview for {fileId}", fileId.ToString());
+            var queryResult = await handler.Process(new GetFileOverviewQueryRequest()
+            {
+                FileId = fileId,
+                Consumer = token.Consumer,
+                Supplier = token.Supplier
+            });
+            return queryResult.Match(
+                result => Ok(FileStatusOverviewExtMapper.MapToExternalModel(result.File)),
+                error => Problem(detail: error.Message, statusCode: (int)error.StatusCode)
+            );
+        }
+
+        /// <summary>
+        /// Get more detailed information about the file upload for auditing and troubleshooting purposes
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{fileId}/details")]
+        public async Task<ActionResult<FileStatusDetailsExt>> GetFileDetails(
+            Guid fileId,
+            [ModelBinder(typeof(MaskinportenModelBinder))] MaskinportenToken token,
+            [FromServices] GetFileDetailsQueryHandler handler)
+        {
+            LogContextHelpers.EnrichLogsWithMaskinporten(token);
+            _logger.LogInformation("Getting file details for {fileId}", fileId.ToString());
+            var queryResult = await handler.Process(new GetFileDetailsQueryRequest()
+            {
+                FileId = fileId,
+                Consumer = token.Consumer,
+                Supplier = token.Supplier
+            });
+            return queryResult.Match(
+                result => Ok(FileStatusDetailsExtMapper.MapToExternalModel(result.File, result.FileEvents, result.ActorEvents)),
+                error => Problem(detail: error.Message, statusCode: (int)error.StatusCode)
+            );
+        }
+
+        /// <summary>
+        /// Get files that can be accessed by the caller according to specified filters. Result set is limited to 100 files.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<List<Guid>>> GetFiles(
+            [FromQuery] FileStatusExt? status,
+            [FromQuery] DateTimeOffset? from,
+            [FromQuery] DateTimeOffset? to,
+            [ModelBinder(typeof(MaskinportenModelBinder))] MaskinportenToken token,
+            [FromServices] GetFilesQueryHandler handler)
+        {
+            LogContextHelpers.EnrichLogsWithMaskinporten(token);
+            _logger.LogInformation("Getting files with status {status} created {from} to {to}", status?.ToString(), from?.ToString(), to?.ToString());
+            var queryResult = await handler.Process(new GetFilesQueryRequest()
+            {
+                Consumer = token.Consumer,
+                Supplier = token.Supplier,
+                Status = status is not null ? (FileStatus)status : null,
+                From = from,
+                To = to
+            });
+            return queryResult.Match(
+                Ok,
+                error => Problem(detail: error.Message, statusCode: (int)error.StatusCode)
+            );
+        }
+
+        /// <summary>
+        /// Downloads the file
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{fileId}/download")]
+        public async Task<ActionResult> DownloadFile(
+            Guid fileId,
+            [ModelBinder(typeof(MaskinportenModelBinder))] MaskinportenToken token,
+            [FromServices] DownloadFileQueryHandler handler)
+        {
+            LogContextHelpers.EnrichLogsWithMaskinporten(token);
+            _logger.LogInformation("Downloading file {fileId}", fileId.ToString());
+            var queryResult = await handler.Process(new DownloadFileQueryRequest()
+            {
+                FileId = fileId,
+                Consumer = token.Consumer,
+                Supplier = token.Supplier
+            });
+            return queryResult.Match<ActionResult>(
+                result => File(result.Stream, "application/octet-stream", result.Filename),
+                error => Problem(detail: error.Message, statusCode: (int)error.StatusCode)
+            );
+        }
+
+        /// <summary>
+        /// Confirms that the file has been downloaded
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{fileId}/confirmdownload")]
+        public async Task<ActionResult> ConfirmDownload(
+            Guid fileId,
+            [ModelBinder(typeof(MaskinportenModelBinder))] MaskinportenToken token,
+            [FromServices] ConfirmDownloadCommandHandler handler)
+        {
+            LogContextHelpers.EnrichLogsWithMaskinporten(token);
+            _logger.LogInformation("Confirming download for file {fileId}", fileId.ToString());
+            var commandResult = await handler.Process(new ConfirmDownloadCommandRequest()
+            {
+                FileId = fileId,
+                Supplier = token.Supplier,
+                Consumer = token.Consumer
+            });
+            return commandResult.Match(
+                Ok,
+                error => Problem(detail: error.Message, statusCode: (int)error.StatusCode)
+            );
+        }
+    }
+}
