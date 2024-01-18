e316b362f02e3d71775b9a0a5e40f42554b3cf21
Ragnar Inge Fatland <acn-rfatl@ai-dev.no>
2024-01-18 18:16:52 +0100

    WIP Commit, failing unit test with 401; need to adjust test token or authorization settings for Legacy.

--- Test/Altinn.Broker.Tests/LegacyFileControllerTests.cs
+++ Test/Altinn.Broker.Tests/LegacyFileControllerTests.cs
@@ -22,12 +22,15 @@ namespace Altinn.Broker.Tests;
 public class LegacyFileControllerTests : IClassFixture<CustomWebApplicationFactory>
 {
     private readonly CustomWebApplicationFactory _factory;
+    private readonly HttpClient _senderClient;
     private readonly HttpClient _legacyClient;
     private readonly JsonSerializerOptions _responseSerializerOptions;
 
     public LegacyFileControllerTests(CustomWebApplicationFactory factory)
     {
         _factory = factory;
+        _senderClient = factory.CreateClient();
+        _senderClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", TestConstants.DUMMY_SENDER_TOKEN);
         _legacyClient = factory.CreateClient();
         _legacyClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", TestConstants.DUMMY_LEGACY_TOKEN);
 
@@ -38,4 +41,31 @@ public class LegacyFileControllerTests : IClassFixture<CustomWebApplicationFacto
         _responseSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     }
 
+    [Fact]
+    public async Task GetFileSentByA3Sender_Success()
+    {
+        // Arrange
+        string status = "Published";
+        DateTimeOffset dateTimeFrom = DateTime.Now.AddMinutes(-2);
+        var initializeFileResponse = await _senderClient.PostAsJsonAsync("broker/api/v1/file", FileInitializeExtTestFactory.BasicFile());
+        Assert.True(initializeFileResponse.IsSuccessStatusCode, await initializeFileResponse.Content.ReadAsStringAsync());
+        DateTimeOffset dateTimeTo = DateTime.Now.AddMinutes(2);
+        var fileId = await initializeFileResponse.Content.ReadAsStringAsync();
+        var initializedFile = await _senderClient.GetFromJsonAsync<FileOverviewExt>($"broker/api/v1/file/{fileId}", _responseSerializerOptions);
+        Assert.NotNull(initializedFile);
+        var uploadedFileBytes = Encoding.UTF8.GetBytes("This is the contents of the uploaded file");
+        using (var content = new ByteArrayContent(uploadedFileBytes))
+        {
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            var uploadResponse = await _senderClient.PostAsync($"broker/api/v1/file/{fileId}/upload", content);
+            Assert.True(uploadResponse.IsSuccessStatusCode, await uploadResponse.Content.ReadAsStringAsync());
+        }
+
+        // Act        
+        var uploadedFile = await _legacyClient.GetFromJsonAsync<FileOverviewExt>($"broker/api/legacy/v1/file/{fileId}?onBehalfOfConsumer={FileInitializeExtTestFactory.BasicFile().Recipients[0]}", _responseSerializerOptions);
+
+        // Assert
+        Assert.NotNull(uploadedFile);
+        Assert.Equal(uploadedFile.FileId.ToString(), fileId);
+    }
 }
