7e8471a1ac9153969715c4977b4d3ab23d9b2368
Roar Mjelde <roarmj@gmail.com>
2024-02-14 16:55:32 +0100

    Updated Postman collection and changed tests to use actual resources, simple for now

--- Test/Altinn.Broker.Tests/Data/R__Prepare_Test_Data.sql
+++ Test/Altinn.Broker.Tests/Data/R__Prepare_Test_Data.sql
@@ -8,22 +8,22 @@ INSERT INTO broker.user (client_id_pk, organization_number)
 VALUES ('00000000-0000-0000-0000-000000000000', '0192:991825827');				 
 
 INSERT INTO broker.user_right (resource_id, user_id_fk, user_right_description_id_fk)
-VALUES ('resource-1', '00000000-0000-0000-0000-000000000000', 1);
+VALUES ('altinn-broker-test-resource-1', '00000000-0000-0000-0000-000000000000', 1);
 
 INSERT INTO broker.user_right (resource_id, user_id_fk, user_right_description_id_fk)
-VALUES ('resource-2', '00000000-0000-0000-0000-000000000000', 1);
+VALUES ('altinn-broker-test-resource-2', '00000000-0000-0000-0000-000000000000', 1);
 
 INSERT INTO broker.user (client_id_pk, organization_number)
 VALUES ('11111111-1111-1111-1111-111111111111', '0192:991825832');				 
 
 INSERT INTO broker.user_right (resource_id, user_id_fk, user_right_description_id_fk)
-VALUES ('resource-1', '11111111-1111-1111-1111-111111111111', 0);
+VALUES ('altinn-broker-test-resource-1', '11111111-1111-1111-1111-111111111111', 0);
 
 INSERT INTO broker.user (client_id_pk, organization_number)
 VALUES ('21111111-2111-2111-2111-211111111111', '0192:910351192');	
 
 INSERT INTO broker.user_right (resource_id, user_id_fk, user_right_description_id_fk)
-VALUES ('resource-2', '11111111-1111-1111-1111-111111111111', 0);
+VALUES ('altinn-broker-test-resource-2', '11111111-1111-1111-1111-111111111111', 0);
 
 INSERT INTO broker.user_right (resource_id, user_id_fk, user_right_description_id_fk)
-VALUES ('resource-2', '21111111-2111-2111-2111-211111111111', 0);
+VALUES ('altinn-broker-test-resource-2', '21111111-2111-2111-2111-211111111111', 0);
