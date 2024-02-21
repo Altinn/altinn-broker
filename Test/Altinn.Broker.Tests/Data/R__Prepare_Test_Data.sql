a23552358276fbbb7494af4386dc8cd4f7845aa9
Roar Mjelde <roarmj@gmail.com>
2024-02-21 14:14:59 +0100

    Fixed test data to use correct storage provider type as malware scan is not run during normal automated E2E

--- Test/Altinn.Broker.Tests/Data/R__Prepare_Test_Data.sql
+++ Test/Altinn.Broker.Tests/Data/R__Prepare_Test_Data.sql
@@ -2,4 +2,4 @@ INSERT INTO broker.resource_owner (resource_owner_id_pk, resource_owner_name, fi
 VALUES ('0192:991825827', 'Digitaliseringsdirektoratet Avd Oslo', '1 Days');
 
 INSERT INTO broker.storage_provider (storage_provider_id_pk, resource_owner_id_fk, created, storage_provider_type, resource_name)
-VALUES (DEFAULT, '0192:991825827', NOW(), 'Altinn3Azure', 'dummy-value');
+VALUES (DEFAULT, '0192:991825827', NOW(), 'Azurite', 'dummy-value');
