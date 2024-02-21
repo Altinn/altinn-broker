f5a56d690035633c276656b0923858bcf1e39d88
Roar Mjelde <roarmj@gmail.com>
2024-02-21 15:31:06 +0100

    Add some very basic auth to resource owner controller to permit external testing

--- src/Altinn.Broker.API/Models/ResourceOwner/ResourceOwnerInitializeExt.cs
+++ src/Altinn.Broker.API/Models/ResourceOwner/ResourceOwnerInitializeExt.cs
@@ -4,11 +4,6 @@ namespace Altinn.Broker.Models.ResourceOwner;
 
 public class ResourceOwnerInitializeExt
 {
-    /// <summary>
-    /// This should be on the form countrycode:organizationnumber. For instance 0192:922444555 for a Norwegian organization with org number 923 444 555. Corresponds to consumer.id in Maskinporten token.
-    /// </summary>
-    [RegularExpressionAttribute(@"^\d{4}:\d{9}$", ErrorMessage = "ResourceOwnerId should be on the Maskinporten form with countrycode:organizationnumber, for instance 0192:910753614")]
-    public string OrganizationId { get; set; }
     public string Name { get; set; }
     public string DeletionTime { get; set; } // ISO8601 Duration
 }
