9a26e51d1417086e1d609adee136015b568ea0da
Hammerbeck <andreas.hammerbeck@digdir.no>
2024-02-28 12:55:10 +0100

    format

--- src/Altinn.Broker.Persistence/DependencyInjection.cs
+++ src/Altinn.Broker.Persistence/DependencyInjection.cs
@@ -6,14 +6,14 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Altinn.Broker.Persistence;
 public static class DependencyInjection
 {
-        public static void AddPersistence(this IServiceCollection services)
-        {
-                services.AddSingleton<DatabaseConnectionProvider>();
-                services.AddSingleton<IActorRepository, ActorRepository>();
-                services.AddSingleton<IFileTransferRepository, FileTransferRepository>();
-                services.AddSingleton<IFileTransferStatusRepository, FileTransferStatusRepository>();
-                services.AddSingleton<IActorFileTransferStatusRepository, ActorFileTransferStatusRepository>();
-                services.AddSingleton<IServiceOwnerRepository, ServiceOwnerRepository>();
-                services.AddSingleton<IIdempotencyEventRepository, IdempotencyEventRepository>();
-        }
+    public static void AddPersistence(this IServiceCollection services)
+    {
+        services.AddSingleton<DatabaseConnectionProvider>();
+        services.AddSingleton<IActorRepository, ActorRepository>();
+        services.AddSingleton<IFileTransferRepository, FileTransferRepository>();
+        services.AddSingleton<IFileTransferStatusRepository, FileTransferStatusRepository>();
+        services.AddSingleton<IActorFileTransferStatusRepository, ActorFileTransferStatusRepository>();
+        services.AddSingleton<IServiceOwnerRepository, ServiceOwnerRepository>();
+        services.AddSingleton<IIdempotencyEventRepository, IdempotencyEventRepository>();
+    }
 }
