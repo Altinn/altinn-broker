7be1001487191df8003066d6910116fb51ec5299
Andreas Hammerbeck <Andreas_93@hotmail.com>
2024-03-06 17:15:05 +0100

    Fix delete blob on storage (#347)
    Co-authored-by: Hammerbeck <andreas.hammerbeck@digdir.no>

--- src/Altinn.Broker.Integrations/Azure/AzureResourceManagerService.cs
+++ src/Altinn.Broker.Integrations/Azure/AzureResourceManagerService.cs
@@ -238,7 +238,7 @@ public class AzureResourceManagerService : IResourceManager
             StartsOn = DateTimeOffset.UtcNow,
             ExpiresOn = DateTimeOffset.UtcNow.AddHours(24),
         };
-        sasBuilder.SetPermissions(BlobSasPermissions.Read | BlobSasPermissions.Create | BlobSasPermissions.List | BlobSasPermissions.Write);
+        sasBuilder.SetPermissions(BlobSasPermissions.Read | BlobSasPermissions.Create | BlobSasPermissions.List | BlobSasPermissions.Write | BlobSasPermissions.Delete);
         string sasToken = sasBuilder.ToSasQueryParameters(credential).ToString();
         _logger.LogInformation("SAS Token created");
         return sasToken;
