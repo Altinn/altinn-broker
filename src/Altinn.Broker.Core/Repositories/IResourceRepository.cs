12afd3268b5beeea9a13df01df0720095f65ddfa
Hammerbeck <andreas.hammerbeck@digdir.no>
2024-03-12 09:47:17 +0100

    Remove inheritance from Repository

--- src/Altinn.Broker.Core/Repositories/IResourceRepository.cs
+++ src/Altinn.Broker.Core/Repositories/IResourceRepository.cs
@@ -4,8 +4,9 @@ using System.Numerics;
 using Altinn.Broker.Core.Domain;
 
 namespace Altinn.Broker.Core.Repositories;
-public interface IResourceRepository : IAltinnResourceRepository
+public interface IResourceRepository
 {
+    Task<ResourceEntity?> GetResource(string resourceId, CancellationToken cancellationToken = default);
     Task UpdateMaxFileTransferSize(string resourceId, long maxSize, CancellationToken cancellationToken = default);
     Task CreateResource(ResourceEntity resource, CancellationToken cancellationToken = default);
 }
