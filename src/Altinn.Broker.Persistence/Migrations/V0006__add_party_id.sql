56f7f1416d294761454c423aa36e468f6aa3c35f
Roar Mjelde <36594318+Ceredron@users.noreply.github.com>
2024-03-25 17:43:02 +0100

    Make column names consistent (#367)
    * Make column names consistent
    
    * Fix elsewhere too, and re-write sql queries to not use *

--- src/Altinn.Broker.Persistence/Migrations/V0006__add_party_id.sql
+++ src/Altinn.Broker.Persistence/Migrations/V0006__add_party_id.sql
@@ -1,5 +1,5 @@
 CREATE TABLE broker.party (
-    organization_number character varying(14) PRIMARY KEY,
+    organization_number_pk character varying(14) PRIMARY KEY,
     party_id character varying(20) NOT NULL,
     created timestamp without time zone NOT NULL
 );
