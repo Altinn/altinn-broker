4f39d72026683cb6637c118d4f12f35b8232409b
Roar Mjelde <roarmj@gmail.com>
2024-03-25 15:46:42 +0100

    Fix elsewhere too, and re-write sql queries to not use *

--- src/Altinn.Broker.Persistence/Migrations/V0005__add_resource_table.sql
+++ src/Altinn.Broker.Persistence/Migrations/V0005__add_resource_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE broker.altinn_resource (
     created timestamp without time zone NOT NULL,
     max_file_transfer_size bigint,
     organization_number character varying(14) NOT NULL,
-    service_owner_id character varying(14) NOT NULL
+    service_owner_id_fk character varying(14) NOT NULL,
+    FOREIGN KEY (service_owner_id_fk) REFERENCES broker.service_owner (service_owner_id_pk)
 );
