e454460877721da5d68ea2df3b411a2698e6ea9d
Andreas Hammerbeck <Andreas_93@hotmail.com>
2024-04-17 15:33:46 +0200

    Custom jwt issuer exception (#397)
    * Add extra info when token issuer is maskinporten token
    
    ---------
    
    Co-authored-by: Hammerbeck <andreas.hammerbeck@digdir.no>

---
+++ src/Altinn.Broker.API/Helpers/JWTBearerEventsHelper.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Altinn.Broker.Helpers;
+public class JWTBearerEventsHelper
+{
+    public static Task OnAuthenticationFailed(AuthenticationFailedContext context)
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        context.Response.ContentType = "application/json";
+        context.Response.Headers.Append("WWW-Authenticate", context.Options.Challenge + " error=\"invalid_token\", error_description=\"" + context.Exception.Message + "\"");
+        string err = "";
+        if (context.Exception is SecurityTokenInvalidIssuerException)
+        {
+            context.Response.StatusCode = StatusCodes.Status403Forbidden;
+            context.Response.ContentType = "application/json";
+            var issuer = ((SecurityTokenInvalidIssuerException)context.Exception).InvalidIssuer;
+            if (issuer.ToString().Contains("maskinporten.no"))
+            {
+                err = "IDX10205: Issuer validation failed. Maskinporten token is not valid. Exchange to Altinn token and try again. Read more at https://docs.altinn.studio/api/scenarios/authentication/#maskinporten-jwt-access-token-input";
+            }
+        }
+        return context.Response.WriteAsync(err);
+    }
+}
