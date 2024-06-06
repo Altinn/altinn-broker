4cc5cb4b527210d83af817dfce044a74322c76f6
Roar Mjelde <roarmj@gmail.com>
2024-06-06 14:10:24 +0200

    urn:altinn:organization:identifier-no is the new urn:altinn:organizationnumber so changed that. But keep using both interchangeabl for now to support existing policies.

--- src/Altinn.Broker.Integrations/Altinn/Authorization/XacmlMappers.cs
+++ src/Altinn.Broker.Integrations/Altinn/Authorization/XacmlMappers.cs
@@ -1,4 +1,7 @@
-﻿using Altinn.Authorization.ABAC.Xacml.JsonProfile;
+﻿using System.Security.Claims;
+using System.Text.RegularExpressions;
+
+using Altinn.Authorization.ABAC.Xacml.JsonProfile;
 using Altinn.Broker.Core.Domain;
 using Altinn.Common.PEP.Constants;
 using Altinn.Common.PEP.Helpers;
@@ -37,9 +40,6 @@ public static class XacmlMappers
     /// </summary>
     internal const string ResourceId = "r";
 
-    /// <summary>
-    /// Creates an action category with the provided action type as an attribute
-    /// </summary>
     /// <param name="actionType">Action type represented as a string</param>
     /// <param name="includeResult">A value indicating whether the value should be included in the result</param>
     /// <returns>A XacmlJsonCategory</returns>
@@ -56,10 +56,6 @@ public static class XacmlMappers
         return actionAttributes;
     }
 
-    /// <summary>
-    /// Creates a resource category for resource.
-    /// </summary>
-    /// <remarks>
     /// If id is required this should be included by the caller. 
     /// Attribute eventId is tagged with `includeInResponse`</remarks>
     internal static XacmlJsonCategory CreateResourceCategory(ResourceEntity resourceEntity)
@@ -70,5 +66,65 @@ public static class XacmlMappers
         resourceCategory.Attribute.Add(DecisionHelper.CreateXacmlJsonAttribute(AltinnXacmlUrns.AppId, "altinn-broker", DefaultType, DefaultIssuer));
 
         return resourceCategory;
+    }
+
+    internal static XacmlJsonCategory CreateSubjectCategory(ClaimsPrincipal user)
+    {
+        XacmlJsonCategory xacmlJsonCategory = new XacmlJsonCategory();
+        List<XacmlJsonAttribute> list = new List<XacmlJsonAttribute>();
+
+        foreach (Claim claim in user.Claims)
+        {
+            if (IsCamelCaseOrgnumberClaim(claim.Type))
+            {
+                list.Add(CreateXacmlJsonAttribute("urn:altinn:organizationnumber", claim.Value, "string", claim.Issuer));
+                list.Add(CreateXacmlJsonAttribute("urn:altinn:organization:identifier-no", claim.Value, "string", claim.Issuer));
+            }
+            else if (IsScopeClaim(claim.Type))
+            {
+                list.Add(CreateXacmlJsonAttribute("urn:scope", claim.Value, "string", claim.Issuer));
+            }
+            else if (IsJtiClaim(claim.Type))
+            {
+                list.Add(CreateXacmlJsonAttribute("urn:altinn:sessionid", claim.Value, "string", claim.Issuer));
+            }
+            else if (IsValidUrn(claim.Type))
+            {
+                list.Add(CreateXacmlJsonAttribute(claim.Type, claim.Value, "string", claim.Issuer));
+            }
+        }
+        xacmlJsonCategory.Attribute = list;
+        return xacmlJsonCategory;
+    }
+    private static bool IsValidUrn(string value)
+    {
+        Regex regex = new Regex("^urn*");
+        return regex.Match(value).Success;
+    }
+
+    private static bool IsCamelCaseOrgnumberClaim(string value)
+    {
+        return value.Equals("urn:altinn:orgNumber");
+    }
+
+    private static bool IsScopeClaim(string value)
+    {
+        return value.Equals("scope");
+    }
+
+    private static bool IsJtiClaim(string value)
+    {
+        return value.Equals("jti");
+    }
+
+    private static XacmlJsonAttribute CreateXacmlJsonAttribute(string attributeId, string value, string dataType, string issuer, bool includeResult = false)
+    {
+        XacmlJsonAttribute xacmlJsonAttribute = new XacmlJsonAttribute();
+        xacmlJsonAttribute.AttributeId = attributeId;
+        xacmlJsonAttribute.Value = value;
+        xacmlJsonAttribute.DataType = dataType;
+        xacmlJsonAttribute.Issuer = issuer;
+        xacmlJsonAttribute.IncludeInResult = includeResult;
+        return xacmlJsonAttribute;
     }
 }
