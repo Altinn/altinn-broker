dc34937fb6f0f149b771d9fd26d40b72b691adc0
Ragnar Inge Fatland <acn-rfatl@ai-dev.no>
2024-06-20 08:33:31 +0200

    Fix merge erorrs

--- src/Altinn.Broker.API/Models/FileTransferInitializeExt.cs
+++ src/Altinn.Broker.API/Models/FileTransferInitializeExt.cs
@@ -64,6 +64,7 @@ public class FileTransferInitalizeExt
     [MD5Checksum]
     public string? Checksum { get; set; } = string.Empty;
 }
+
 internal class MD5ChecksumAttribute : ValidationAttribute
 {
     public MD5ChecksumAttribute()
@@ -87,41 +88,41 @@ internal class MD5ChecksumAttribute : ValidationAttribute
         }
         return ValidationResult.Success!;
     }
+}
 
-    [AttributeUsage(AttributeTargets.Property)]
-    internal class PropertyListAttribute : ValidationAttribute
+[AttributeUsage(AttributeTargets.Property)]
+internal class PropertyListAttribute : ValidationAttribute
+{
+    public PropertyListAttribute()
     {
-        public PropertyListAttribute()
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if(value == null) 
         {
-        }
-
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if(value == null) 
-            {
-                return ValidationResult.Success;
-            }
-
-            if (!(value is Dictionary<string, string>))
-            {
-                return new ValidationResult("PropertyList Object is not of proper type");
-            }
-
-            var dictionary = (Dictionary<string, string>)value;
-
-            if (dictionary.Count > 10)
-                return new ValidationResult("PropertyList can contain at most 10 properties");
-
-            foreach (var keyValuePair in dictionary)
-            {
-                if (keyValuePair.Key.Length > 50)
-                    return new ValidationResult(String.Format("PropertyList Key can not be longer than 50. Length:{0}, KeyValue:{1}", keyValuePair.Key.Length.ToString(), keyValuePair.Key));
-
-                if (keyValuePair.Value.Length > 300)
-                    return new ValidationResult(String.Format("PropertyList Value can not be longer than 300. Length:{0}, Value:{1}", keyValuePair.Value.Length.ToString(), keyValuePair.Value));
-            }
-
             return ValidationResult.Success;
         }
+
+        if (!(value is Dictionary<string, string>))
+        {
+            return new ValidationResult("PropertyList Object is not of proper type");
+        }
+
+        var dictionary = (Dictionary<string, string>)value;
+
+        if (dictionary.Count > 10)
+            return new ValidationResult("PropertyList can contain at most 10 properties");
+
+        foreach (var keyValuePair in dictionary)
+        {
+            if (keyValuePair.Key.Length > 50)
+                return new ValidationResult(String.Format("PropertyList Key can not be longer than 50. Length:{0}, KeyValue:{1}", keyValuePair.Key.Length.ToString(), keyValuePair.Key));
+
+            if (keyValuePair.Value.Length > 300)
+                return new ValidationResult(String.Format("PropertyList Value can not be longer than 300. Length:{0}, Value:{1}", keyValuePair.Value.Length.ToString(), keyValuePair.Value));
+        }
+
+        return ValidationResult.Success;
     }
 }
