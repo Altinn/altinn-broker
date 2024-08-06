7f9522ef87c19483206c106c7d32561cafc65bda
CelineTrammi <61122289+CelineTrammi@users.noreply.github.com>
2024-08-06 14:09:10 +0200

    replace request.ResourceId with resource.Id (#514)

--- src/Altinn.Broker.Application/InitializeFileTransferCommand/InitializeFileTransferCommandHandler.cs
+++ src/Altinn.Broker.Application/InitializeFileTransferCommand/InitializeFileTransferCommandHandler.cs
@@ -85,7 +85,7 @@ public class InitializeFileTransferCommandHandler : IHandler<InitializeFileTrans
             Force = false
         }, cancellationToken), fileExpirationTime);
         await _fileTransferRepository.SetFileTransferHangfireJobId(fileTransferId, jobId, cancellationToken);
-        await _eventBus.Publish(AltinnEventType.FileTransferInitialized, request.ResourceId, fileTransferId.ToString(), request.SenderExternalId, cancellationToken);
+        await _eventBus.Publish(AltinnEventType.FileTransferInitialized, resource.Id, fileTransferId.ToString(), request.SenderExternalId, cancellationToken);
 
         return fileTransferId;
     }
