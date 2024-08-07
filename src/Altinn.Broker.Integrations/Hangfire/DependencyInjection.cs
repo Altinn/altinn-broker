3c1074f2b73bdf12f829398fc917b51c53ab35b9
Roar Mjelde <roarmj@gmail.com>
2024-08-07 10:51:39 +0200

    More fix

--- src/Altinn.Broker.Integrations/Hangfire/DependencyInjection.cs
+++ src/Altinn.Broker.Integrations/Hangfire/DependencyInjection.cs
@@ -16,9 +16,10 @@ public static class DependencyInjection
         var databaseOptions = new DatabaseOptions() { ConnectionString = "" };
         config.GetSection(nameof(DatabaseOptions)).Bind(databaseOptions);
         var serviceProvider = services.BuildServiceProvider();
-        var connectionFactory = serviceProvider.GetRequiredService<IConnectionFactory>();
         services.AddHangfire(config =>
-            config.UsePostgreSqlStorage(options => options.UseConnectionFactory(connectionFactory))
+            config.UsePostgreSqlStorage(
+                c => c.UseConnectionFactory(serviceProvider.GetRequiredService<DatabaseConnectionProvider>())
+            )
         );
         services.AddHangfireServer();
     }
