cc597c2d423cec8d432d354de37198a5d51b4bf7
Roar Mjelde <roarmj@gmail.com>
2024-08-12 12:48:07 +0200

    Script clean-up

--- src/Altinn.Broker.Persistence/Migrations/V0007__change_hangfire_owner.sql
+++ src/Altinn.Broker.Persistence/Migrations/V0007__change_hangfire_owner.sql
@@ -1,20 +1,34 @@
 ﻿ALTER SCHEMA hangfire OWNER TO azure_pg_admin;
 
-CREATE OR REPLACE FUNCTION set_table_owner()
-RETURNS event_trigger AS $$
+CREATE OR REPLACE FUNCTION set_hangfire_table_owner()
+RETURNS event_trigger
+LANGUAGE plpgsql
+AS $$
 DECLARE
-  obj record;
+    obj record;
+    target_role name := 'azure_pg_admin';
+    target_schema name := 'hangfire';
 BEGIN
-  FOR obj IN SELECT * FROM pg_event_trigger_ddl_commands() 
-    WHERE command_tag = 'CREATE TABLE' 
-    AND object_identity ~ '^hangfire\.'
-  LOOP
-    EXECUTE format('ALTER TABLE %s OWNER TO azure_pg_admin', obj.object_identity);
-END LOOP;
+    FOR obj IN 
+        SELECT * FROM pg_event_trigger_ddl_commands()
+        WHERE command_tag = 'CREATE TABLE'
+          AND object_schema = target_schema
+    LOOP
+        EXECUTE format(
+            'ALTER TABLE %I.%I OWNER TO %I',
+            obj.object_schema,
+            obj.object_name,
+            target_role
+        );
+        
+        RAISE NOTICE 'Changed owner of %.% to %', 
+            obj.object_schema, obj.object_name, target_role;
+    END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
-CREATE EVENT TRIGGER set_table_owner_trigger
+-- Create the event trigger
+CREATE EVENT TRIGGER set_hangfire_table_owner_trigger
 ON ddl_command_end
 WHEN TAG IN ('CREATE TABLE')
-EXECUTE FUNCTION set_table_owner();
+EXECUTE FUNCTION set_hangfire_table_owner();
