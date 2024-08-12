a6e10e07ab55cd59ddb9916bacfdac9692c22924
Roar Mjelde <roarmj@gmail.com>
2024-08-12 12:19:28 +0200

    The Hangfire tables are created by the application. This leads to the application identity becoming the owner of said tables - limiting our access when troubleshooting. Instead, we set the owner to the admin role in the database.

---
+++ src/Altinn.Broker.Persistence/Migrations/V0007__change_hangfire_owner.sql
@@ -0,0 +1,20 @@
+﻿ALTER SCHEMA hangfire OWNER TO azure_pg_admin;
+
+CREATE OR REPLACE FUNCTION set_table_owner()
+RETURNS event_trigger AS $$
+DECLARE
+  obj record;
+BEGIN
+  FOR obj IN SELECT * FROM pg_event_trigger_ddl_commands() 
+    WHERE command_tag = 'CREATE TABLE' 
+    AND object_identity ~ '^hangfire\.'
+  LOOP
+    EXECUTE format('ALTER TABLE %s OWNER TO azure_pg_admin', obj.object_identity);
+END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE EVENT TRIGGER set_table_owner_trigger
+ON ddl_command_end
+WHEN TAG IN ('CREATE TABLE')
+EXECUTE FUNCTION set_table_owner();
