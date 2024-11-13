1c52161b943398b98a21987fd82f105b19ff18b4
Roar Mjelde <roarmj@gmail.com>
2024-11-13 21:50:19 +0100

    Increased to theoretical max

--- src/Altinn.Broker.Application/ApplicationConstants.cs
+++ src/Altinn.Broker.Application/ApplicationConstants.cs
@@ -2,7 +2,7 @@ namespace Altinn.Broker.Application.Settings;
 
 public static class ApplicationConstants
 {
-    public const long MaxFileUploadSize = 100L * 1024 * 1024 * 1024;
+    public const long MaxFileUploadSize = 32L * 50000 * 1024 * 1024;
     public const long MaxVirusScanUploadSize = 2L * 1024 * 1024 * 1024;
     public const string DefaultGracePeriod = "PT2H";
     public const string MaxGracePeriod = "PT24H";
