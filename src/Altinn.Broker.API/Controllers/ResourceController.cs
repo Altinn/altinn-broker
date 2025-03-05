ad12fc8481f49f0659e9bfba69dd6fcf12fc4183
Roar Mjelde <roarmj@gmail.com>
2025-03-05 20:41:44 +0100

    Implemented functionality to get resource information

--- src/Altinn.Broker.API/Controllers/ResourceController.cs
+++ src/Altinn.Broker.API/Controllers/ResourceController.cs
@@ -1,6 +1,7 @@
 using Altinn.Broker.API.Configuration;
 using Altinn.Broker.Application;
 using Altinn.Broker.Application.ConfigureResource;
+using Altinn.Broker.Application.GetResource;
 using Altinn.Broker.Models;
 
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -47,6 +48,41 @@ public class ResourceController : Controller
 
         return result.Match(
             (_) => Ok(null),
+            Problem
+        );
+    }
+
+    /// <summary>
+    /// Gets information about a resource configuration in broker
+    /// </summary>
+    /// <remarks>
+    /// Scopes: <br/> 
+    /// - altinn:serviceowner <br/>
+    /// </remarks>
+    /// <returns></returns>
+    [HttpGet]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [Route("{resourceId}")]
+    public async Task<ActionResult> GetResource(string resourceId, [FromServices] GetResourceHandler handler, CancellationToken cancellationToken)
+    {
+        var result = await handler.Process(resourceId, HttpContext.User, cancellationToken);
+
+        return result.Match(
+            (resource) => Ok(new ResourceExt()
+            {
+                ExternalServiceCodeLegacy = resource.ExternalServiceCodeLegacy,
+                ExternalServiceEditionCodeLegacy = resource.ExternalServiceEditionCodeLegacy,
+                FileTransferTimeToLive = resource.FileTransferTimeToLive.HasValue ? resource.FileTransferTimeToLive.Value.ToString() : null,
+                MaxFileTransferSize = resource.MaxFileTransferSize,
+                PurgeFileTransferAfterAllRecipientsConfirmed = resource.PurgeFileTransferAfterAllRecipientsConfirmed,
+                PurgeFileTransferGracePeriod = resource.PurgeFileTransferGracePeriod.HasValue ? resource.PurgeFileTransferGracePeriod.Value.ToString() : null,
+                UseManifestFileShim = resource.UseManifestFileShim
+            }),
             Problem
         );
     }
