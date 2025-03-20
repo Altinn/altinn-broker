cf5fd08ce3180d84512f3f344388d0c34748e4a3
Andreas Hammerbeck <Andreas_93@hotmail.com>
2025-03-20 10:23:26 +0100

    add detailed explaination of errors (#680)
    Co-authored-by: Hammerbeck <andreas.hammerbeck@digdir.no>

--- src/Altinn.Broker.API/Controllers/ResourceController.cs
+++ src/Altinn.Broker.API/Controllers/ResourceController.cs
@@ -20,10 +20,22 @@ public class ResourceController : Controller
     /// Configures a resource with settings to be used within the broker service.
     /// </summary>
     /// <remarks>
-    /// Scopes: <br/> 
+    /// One of the scopes: <br/> 
     /// - altinn:serviceowner <br/>
     /// </remarks>
-    /// <returns></returns>
+    /// <response code="200">Resource configured successfully</response>
+    /// <response code="400"><ul>
+    /// <li>Invalid grace period format. Should follow ISO8601 standard for duration. Example: 'PT2H' for 2 hours</li>
+    /// <li>Grace period cannot exceed 24 hours</li>
+    /// <li>Max file transfer size cannot be negative</li>
+    /// <li>Max file transfer size cannot be zero</li>
+    /// <li>Max file transfer size cannot be set higher than the 2GB in production unless the resource has been pre-approved for disabled virus scan. Contact us @ Slack</li>
+    /// <li>Max file transfer size cannot be set higher than 100GB in production because it has not yet been tested for it. Contact us @ Slack if you need it</li>
+    /// <li>Invalid file transfer time to live format. Should follow ISO8601 standard for duration. Example: 'P30D' for 30 days</li>
+    /// <li>Time to live cannot exceed 365 days</li>
+    /// </ul></response>
+    /// <response code="401">You must use a bearer token that represents a system user with access to the resource in the Resource Rights Registry</response>
+    /// <response code="403">The resource needs to be registered as an Altinn 3 resource and it has to be associated with a service owner</response>
     [HttpPut]
     [Produces("application/json")]
     [Consumes("application/json")]
@@ -56,15 +68,16 @@ public class ResourceController : Controller
     /// Gets information about a resource configuration in broker
     /// </summary>
     /// <remarks>
-    /// Scopes: <br/> 
+    /// One of the scopes: <br/> 
     /// - altinn:serviceowner <br/>
     /// </remarks>
-    /// <returns></returns>
+    /// <response code="200">Detailed information about the resource</response>
+    /// <response code="401">You must use a bearer token that represents a system user with access to the resource in the Resource Rights Registry</response>
+    /// <response code="403">The resource needs to be registered as an Altinn 3 resource and it has to be associated with a service owner</response>
     [HttpGet]
     [Produces("application/json")]
     [Consumes("application/json")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [Route("{resourceId}")]
