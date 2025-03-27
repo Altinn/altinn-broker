8222271fec135db08a827767ed452bab18f8ed90
Ytterås <axel.ytteras@digdir.no>
2025-03-27 10:42:55 +0100

    Added unit tests for the fileTransferMonitorHandler class

---
+++ tests/Altinn.Broker.Tests/FileTransferMonitorHandlerTests.cs
@@ -0,0 +1,128 @@
+using System.Threading.Tasks;
+
+using Altinn.Broker.Application.FileTransferMonitor;
+using Altinn.Broker.Core.Domain;
+using Altinn.Broker.Core.Repositories;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using Slack.Webhooks;
+
+using Xunit;
+
+namespace Altinn.Broker.Tests;
+
+public class FileTransferMonitorHandlerTests
+{
+    [Fact]
+    public async Task CheckForStuckFileTransfers_WhenCalled_LogsInformation()
+    {
+        // Arrange
+        var fileTransferStatusRepository = new Mock<IFileTransferStatusRepository>();
+        var slackClient = new Mock<ISlackClient>();
+        var monitorLogger = new Mock<ILogger<FileTransferMonitorHandler>>();
+        var notifierLogger = new Mock<ILogger<SlackStuckFileTransferNotification>>();
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(e => e.EnvironmentName).Returns("Test");
+        var slackNotifier = new SlackStuckFileTransferNotification(notifierLogger.Object, slackClient.Object, hostEnvironment.Object);
+        var handler = new FileTransferMonitorHandler(fileTransferStatusRepository.Object, slackNotifier, monitorLogger.Object);
+        var cancellationToken = new CancellationToken();
+        fileTransferStatusRepository.Setup(r => r
+            .GetCurrentFileTransferStatusesOfStatusAndOlderThanDate(It.IsAny<Core.Domain.Enums.FileTransferStatus>(), It.IsAny<DateTime>(), cancellationToken))
+            .ReturnsAsync(new List<FileTransferStatusEntity>());
+
+        // Act
+        await handler.CheckForStuckFileTransfers(cancellationToken);
+
+        // Assert
+        monitorLogger.Verify(l => l.Log(
+            LogLevel.Information,
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((v, t) => v.ToString() == "Checking for file transfers stuck in upload processing"),
+            It.IsAny<Exception>(),
+            (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CheckForStuckFileTransfers_WhenStuckFileTransfers_SendsSlackNotification()
+    {
+        // Arrange
+        var fileTransferStatusRepository = new Mock<IFileTransferStatusRepository>();
+        var slackClient = new Mock<ISlackClient>();
+        var monitorLogger = new Mock<ILogger<FileTransferMonitorHandler>>();
+        var notifierLogger = new Mock<ILogger<SlackStuckFileTransferNotification>>();
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(e => e.EnvironmentName).Returns("Test");
+        var slackNotifier = new SlackStuckFileTransferNotification(notifierLogger.Object, slackClient.Object, hostEnvironment.Object);
+        var handler = new FileTransferMonitorHandler(fileTransferStatusRepository.Object, slackNotifier, monitorLogger.Object);
+        var cancellationToken = new CancellationToken();
+        var fileTransferId = Guid.NewGuid();
+        var stuckfileTransferStatuses = new List<FileTransferStatusEntity>()
+        {
+            new FileTransferStatusEntity()
+            {
+                FileTransferId = fileTransferId,
+                Status = Core.Domain.Enums.FileTransferStatus.UploadProcessing,
+                Date = DateTime.UtcNow.AddMinutes(-16)
+            }
+        };
+        fileTransferStatusRepository.Setup(r => r
+            .GetCurrentFileTransferStatusesOfStatusAndOlderThanDate(It.IsAny<Core.Domain.Enums.FileTransferStatus>(), It.IsAny<DateTime>(), cancellationToken))
+            .ReturnsAsync(stuckfileTransferStatuses);
+
+        // Act
+        await handler.CheckForStuckFileTransfers(cancellationToken);
+
+        // Assert
+        monitorLogger.Verify(l => l.Log(
+            LogLevel.Warning,
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((v, t) => v.ToString() == $"File transfer {fileTransferId} has been stuck in upload processing for more than 15 minutes"),
+            It.IsAny<Exception>(),
+            (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CheckForStuckFileTransfers_WhenStuckFileTransfersAlreadyNotified_DoesNotResendSlackNotification()
+    {
+        // Arrange
+        var fileTransferStatusRepository = new Mock<IFileTransferStatusRepository>();
+        var slackClient = new Mock<ISlackClient>();
+        var monitorLogger = new Mock<ILogger<FileTransferMonitorHandler>>();
+        var notifierLogger = new Mock<ILogger<SlackStuckFileTransferNotification>>();
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(e => e.EnvironmentName).Returns("Test");
+        var slackNotifier = new SlackStuckFileTransferNotification(notifierLogger.Object, slackClient.Object, hostEnvironment.Object);
+        var handler = new FileTransferMonitorHandler(fileTransferStatusRepository.Object, slackNotifier, monitorLogger.Object);
+        var cancellationToken = new CancellationToken();
+        var fileTransferId = Guid.NewGuid();
+        var stuckfileTransferStatuses = new List<FileTransferStatusEntity>()
+        {
+            new FileTransferStatusEntity()
+            {
+                FileTransferId = fileTransferId,
+                Status = Core.Domain.Enums.FileTransferStatus.UploadProcessing,
+                Date = DateTime.UtcNow.AddMinutes(-16)
+            }
+        };
+        fileTransferStatusRepository.Setup(r => r
+            .GetCurrentFileTransferStatusesOfStatusAndOlderThanDate(It.IsAny<Core.Domain.Enums.FileTransferStatus>(), It.IsAny<DateTime>(), cancellationToken))
+            .ReturnsAsync(stuckfileTransferStatuses);
+
+        // Act
+        await handler.CheckForStuckFileTransfers(cancellationToken);
+        await handler.CheckForStuckFileTransfers(cancellationToken);
+        await handler.CheckForStuckFileTransfers(cancellationToken);
+
+        // Assert
+        monitorLogger.Verify(l => l.Log(
+            LogLevel.Warning,
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((v, t) => v.ToString() == $"File transfer {fileTransferId} has been stuck in upload processing for more than 15 minutes"),
+            It.IsAny<Exception>(),
+            (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()), Times.Once);
+    }
+}
