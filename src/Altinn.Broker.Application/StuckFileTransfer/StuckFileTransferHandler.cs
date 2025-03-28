8a4af95027125b1615fa5ee3945779512e38dd45
Ytterås <axel.ytteras@digdir.no>
2025-03-28 09:23:41 +0100

    Removed _onGoingStuckFileTransferIds because the class is scoped to avoid any potential memory-related issues

--- src/Altinn.Broker.Application/StuckFileTransfer/StuckFileTransferHandler.cs
+++ src/Altinn.Broker.Application/StuckFileTransfer/StuckFileTransferHandler.cs
@@ -9,7 +9,6 @@ public class StuckFileTransferHandler
 {
     private readonly IFileTransferStatusRepository _fileTransferStatusRepository;
     private readonly ILogger<StuckFileTransferHandler> _logger;
-    private readonly HashSet<Guid> _ongoingStuckFileTransferIds = new HashSet<Guid>();
     private readonly SlackStuckFileTransferNotifier _slackNotifier;
     private readonly int _stuckThresholdMinutes = 5;
 
@@ -33,7 +32,7 @@ public class StuckFileTransferHandler
 
         if (fileTransfersStuckInUploadProcessing.Count == 0)
         {
-            _logger.LogInformation("No file transfers are stuck in upload processing, creating a fictional filetransferEntity to test slack notification");
+            _logger.LogInformation("No file transfers are stuck in upload processing, creating a fictional filetransferEntity to test slack notification of stuck file transfer");
             var fictionalFileTransferStatusEntity = new FileTransferStatusEntity
             {
                 FileTransferId = Guid.NewGuid(),
@@ -45,30 +44,12 @@ public class StuckFileTransferHandler
 
         foreach (FileTransferStatusEntity status in fileTransfersStuckInUploadProcessing)
         {
-            if (!_ongoingStuckFileTransferIds.Contains(status.FileTransferId))
+            _logger.LogWarning("File transfer {fileTransferId} has been stuck in upload processing for more than {thresholdMinutes} minutes", status.FileTransferId, _stuckThresholdMinutes);
+            var succesfullNotification = await _slackNotifier.NotifyFileStuckWithStatus(status);
+            if (!succesfullNotification)
             {
-                _logger.LogWarning("File transfer {fileTransferId} has been stuck in upload processing for more than {thresholdMinutes} minutes", status.FileTransferId, _stuckThresholdMinutes);
-                _ongoingStuckFileTransferIds.Add(status.FileTransferId);
-                var succesfullNotification = await _slackNotifier.NotifyFileStuckWithStatus(status);
-                if (!succesfullNotification)
-                {
-                    _logger.LogError("Failed to send Slack notification for file transfer {fileTransferId}", status.FileTransferId);
-                }
+                _logger.LogError("Failed to send Slack notification for file transfer {fileTransferId}", status.FileTransferId);
             }
-        }
-
-        HashSet<Guid> transferIdsToRemove = new HashSet<Guid>();
-        foreach (Guid fileTransferId in _ongoingStuckFileTransferIds)
-        {
-            if (!fileTransfersStuckInUploadProcessing.Any(s => s.FileTransferId == fileTransferId))
-            {
-                _logger.LogInformation("File transfer {fileTransferId} is no longer stuck in upload processing", fileTransferId);
-                transferIdsToRemove.Add(fileTransferId);
-             }
-        }
-        foreach (Guid idToRemove in transferIdsToRemove)
-        {
-            _ongoingStuckFileTransferIds.Remove(idToRemove);
         }
     }
 }
