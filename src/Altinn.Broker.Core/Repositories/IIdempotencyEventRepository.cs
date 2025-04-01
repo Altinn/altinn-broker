3b2c392dab38681bb2f15d74d29ce237e05a0a51
Roar Mjelde <roarmj@gmail.com>
2025-04-01 15:11:29 +0200

    Use idempotency event in transaction

---
+++ src/Altinn.Broker.Core/Repositories/IIdempotencyEventRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Altinn.Broker.Core.Repositories;
+
+public interface IIdempotencyEventRepository
+{
+    Task AddIdempotencyEventAsync(string id, CancellationToken cancellationToken);
+}
---
+++ src/Altinn.Broker.Persistence/Repositories/IdempotencyEventRepository.cs
@@ -0,0 +1,19 @@
+﻿using Altinn.Broker.Core.Repositories;
+
+using Npgsql;
+
+namespace Altinn.Broker.Persistence.Repositories;
+
+public class IdempotencyEventRepository(NpgsqlDataSource dataSource) : IIdempotencyEventRepository
+{
+    public async Task AddIdempotencyEventAsync(string IdempotencyEventId, CancellationToken cancellationToken)
+    {
+        await using NpgsqlCommand command = dataSource.CreateCommand(
+                    "INSERT INTO broker.idempotency_event (idempotency_event_id_pk, created)" +
+                    "VALUES (@idempotency_event_id_pk, @created) ");
+        command.Parameters.AddWithValue("@idempotency_event_id_pk", IdempotencyEventId);
+        command.Parameters.AddWithValue("@created", DateTime.UtcNow);
+
+        await command.ExecuteNonQueryAsync(cancellationToken);
+    }
+}
