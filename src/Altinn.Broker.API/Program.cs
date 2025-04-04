6737e617dbb14a72aa43f2a304be3b46cfa33118
Ytterås <axel.ytteras@digdir.no>
2025-04-04 13:46:16 +0200

    re-enabled adaptive sampling

--- src/Altinn.Broker.API/Program.cs
+++ src/Altinn.Broker.API/Program.cs
@@ -87,7 +87,7 @@ static void ConfigureServices(IServiceCollection services, IConfiguration config
         var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
         options.IncludeXmlComments(xmlPath);
     });
-    services.AddApplicationInsightsTelemetry(config => config.EnableAdaptiveSampling = false);
+    services.AddApplicationInsightsTelemetry();
     services.AddExceptionHandler<SlackExceptionNotification>();
 
     services.Configure<DatabaseOptions>(config.GetSection(key: nameof(DatabaseOptions)));
