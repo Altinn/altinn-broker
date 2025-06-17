7888b85a10c408d42fe3e607617e231ad131a676
Ytterås <axel.ytteras@digdir.no>
2025-06-17 10:11:00 +0200

    Created ResourceIdentifier attribute which works with and without urn format

---
+++ src/Altinn.Broker.API/ValidationAttributes/ResourceIdentifierAttribute.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Altinn.Broker.Common.Constants;
+
+namespace Altinn.Broker.API.Models;
+
+public class ResourceIdentifierAttribute : ValidationAttribute
+{
+    private static readonly string Pattern = $@"^(?:{UrnConstants.Resource}:)?[^:]{{1,255}}$";
+    private static readonly Regex Regex = new(Pattern);
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not string stringValue || !IsValidResourceFormat(stringValue))
+        {
+            return new ValidationResult(ErrorMessage ?? "Invalid Resource identifier format");
+        }
+
+        return ValidationResult.Success;
+    }
+
+    public static bool IsValidResourceFormat(string value)
+    {
+        return !string.IsNullOrEmpty(value) && Regex.IsMatch(value);
+    }
+}
