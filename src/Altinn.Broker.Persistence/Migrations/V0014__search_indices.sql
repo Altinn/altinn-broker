66a0d1f79029c4fbf99c9b57db98b40a7d631c85
Roar Mjelde <roarmj@gmail.com>
2025-07-23 10:04:27 +0200

    Truncated index names

--- src/Altinn.Broker.Persistence/Migrations/V0014__search_indices.sql
+++ src/Altinn.Broker.Persistence/Migrations/V0014__search_indices.sql
@@ -1,11 +1,11 @@
-﻿CREATE INDEX actor_file_transfer_status_file_transfer_id_fk_actor_id_fk_idx 
+﻿CREATE INDEX actor_file_transfer_actor_lookup_idx 
 ON broker.actor_file_transfer_status (
     file_transfer_id_fk,
     actor_id_fk,
     actor_file_transfer_status_description_id_fk DESC
 );
 
-CREATE INDEX file_transfer_status_file_transfer_status_id_pk_file_transfer_id_fk_idx 
+CREATE INDEX file_transfer_status_file_transfer_id_fk_idx 
 ON broker.file_transfer_status (
     file_transfer_id_fk,
     file_transfer_status_id_pk DESC
