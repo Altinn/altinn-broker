8f90ac8e015683a078d5d64e12c549c85400c481
Roar Mjelde <roarmj@gmail.com>
2025-07-23 09:57:22 +0200

    Optimized broker file transfer search

---
+++ src/Altinn.Broker.Persistence/Migrations/V0014__search_indices.sql
@@ -0,0 +1,18 @@
+﻿CREATE INDEX actor_file_transfer_status_file_transfer_id_fk_actor_id_fk_idx 
+ON broker.actor_file_transfer_status (
+    file_transfer_id_fk,
+    actor_id_fk,
+    actor_file_transfer_status_description_id_fk DESC
+);
+
+CREATE INDEX file_transfer_status_file_transfer_status_id_pk_file_transfer_id_fk_idx 
+ON broker.file_transfer_status (
+    file_transfer_id_fk,
+    file_transfer_status_id_pk DESC
+);
+
+CREATE INDEX file_transfer_file_transfer_id_pk_resource_id_idx 
+ON broker.file_transfer (
+    file_transfer_id_pk,
+    resource_id
+);
