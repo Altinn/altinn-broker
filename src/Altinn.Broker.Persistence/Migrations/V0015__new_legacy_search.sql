573a0c9c7e42e7a2d133e382677b3ad1a45d10a4
Roar Mjelde <roarmj@gmail.com>
2025-09-10 17:31:35 +0200

    Add related indices

---
+++ src/Altinn.Broker.Persistence/Migrations/V0015__new_legacy_search.sql
@@ -0,0 +1,12 @@
+﻿-- For actor file transfer status CTE
+CREATE index CONCURRENTLY idx_actor_file_transfer_status_window 
+ON broker.actor_file_transfer_status (file_transfer_id_fk, actor_id_fk, actor_file_transfer_status_id_pk DESC);
+
+-- For file transfer status CTE  
+CREATE INDEX concurrently idx_file_transfer_status_window
+ON broker.file_transfer_status (file_transfer_id_fk, file_transfer_status_id_pk DESC);
+
+-- For main query filtering
+CREATE INDEX concurrently idx_file_transfer_search
+ON broker.file_transfer (created, resource_id) 
+INCLUDE (file_transfer_id_pk);
