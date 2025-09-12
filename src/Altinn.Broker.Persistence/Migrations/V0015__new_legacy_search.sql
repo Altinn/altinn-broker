8c6796bf3d84f820236e3bcfebe57010227a1800
Roar Mjelde <roarmj@gmail.com>
2025-09-12 13:21:25 +0200

    Locks locally and in pipeline on concurrently

--- src/Altinn.Broker.Persistence/Migrations/V0015__new_legacy_search.sql
+++ src/Altinn.Broker.Persistence/Migrations/V0015__new_legacy_search.sql
@@ -1,12 +1,12 @@
 ﻿-- For actor file transfer status
-CREATE index CONCURRENTLY idx_actor_file_transfer_status_window 
+CREATE index idx_actor_file_transfer_status_window 
 ON broker.actor_file_transfer_status (file_transfer_id_fk, actor_id_fk, actor_file_transfer_status_id_pk DESC);
 
 -- For file transfer status
-CREATE INDEX concurrently idx_file_transfer_status_window
+CREATE INDEX idx_file_transfer_status_window
 ON broker.file_transfer_status (file_transfer_id_fk, file_transfer_status_id_pk DESC);
 
 -- For main query filtering
-CREATE INDEX concurrently idx_file_transfer_search
+CREATE INDEX idx_file_transfer_search
 ON broker.file_transfer (created, resource_id) 
 INCLUDE (file_transfer_id_pk);
