19119684fc5c35f0dc0247056240584320ee06dc
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2025-11-27 10:41:35 +0100

    add response and ihandler

---
+++ src/Altinn.Broker.Application/CleanupUseCaseTests/CleanupUseCaseTestsResponse.cs
@@ -0,0 +1,8 @@
+namespace Altinn.Broker.Application.CleanupUseCaseTests;
+
+public class CleanupUseCaseTestsResponse
+{
+    public required string ResourceId { get; set; }
+    public required int FileTransfersFound { get; set; }
+    public required string DeleteFileTransfersJobId { get; set; } 
+}
