a6b181ebbe87d642a523f9b408a912db68b48720
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2026-02-12 13:26:17 +0100

    remove exceptions file, new implementation throw error directly

--- src/Altinn.Broker.Core/Exceptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Altinn.Broker.Core.Exceptions;
-
-public class ServiceOwnerNotConfiguredException : Exception
-{
-    public ServiceOwnerNotConfiguredException(string serviceOwnerId) : base($"Service owner with id {serviceOwnerId} has not been configured.")
-    {
-    }
-}
