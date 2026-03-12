60ac75faf904d42c132f729ddf0312edeb983c17
Axel Ytterås <axel.ytteraas@gmail.com>
2026-03-12 15:57:10 +0100

    Can not be done concurrently in transaction

--- src/Altinn.Broker.Persistence/Migrations/V0021__file_transfer_status_status_time_index.sql
+++ src/Altinn.Broker.Persistence/Migrations/V0021__file_transfer_status_status_time_index.sql
@@ -1,4 +1,4 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_file_transfer_status_lookup
+CREATE INDEX IF NOT EXISTS idx_file_transfer_status_lookup
 ON broker.file_transfer_status (
     file_transfer_id_fk,
     file_transfer_status_description_id_fk,
