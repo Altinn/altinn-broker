69f4efd6cc5977874c3c868a7c363ab33db0d2dd
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2026-03-23 09:54:07 +0100

    Revert "map legacy format to urn"

--- src/Altinn.Broker.Application/InitializeFileTransfer/InitializeFileTransferHandler.cs
+++ src/Altinn.Broker.Application/InitializeFileTransfer/InitializeFileTransferHandler.cs
@@ -46,20 +46,16 @@ public class InitializeFileTransferHandler(
             logger.LogInformation("Sender using legacy format: {sender}", request.SenderExternalId.SanitizeForLogs());
         }
 
-        List<string> mappedRecipients = new List<string>();
         // Log recipients format
         foreach (var recipient in request.RecipientExternalIds)
         {
             if (recipient.StartsWith("urn:altinn:organization:identifier-no:"))
             {
                 logger.LogInformation("Recipient using new URN format: {recipient}", recipient.SanitizeForLogs());
-                mappedRecipients.Add(recipient);
             }
             else if (recipient.StartsWith("0192:"))
             {
-                logger.LogInformation("Recipient using legacy format: {recipient}, mapping to URN", recipient.SanitizeForLogs());
-                var mappedRecipient = recipient.WithoutPrefix().WithUrnPrefix();
-                mappedRecipients.Add(mappedRecipient);
+                logger.LogInformation("Recipient using legacy format: {recipient}", recipient.SanitizeForLogs());
             }
         }
 
@@ -96,7 +92,7 @@ public class InitializeFileTransferHandler(
         }
         if (altinnResource.AccessListEnabled)
         {
-            foreach (var recipient in mappedRecipients)
+            foreach (var recipient in request.RecipientExternalIds)
             {
                 var accessList = await altinnResourceRepository.GetAccessListOfResource(request.ResourceId, recipient, cancellationToken);
                 if (accessList is null || accessList.Count == 0)
