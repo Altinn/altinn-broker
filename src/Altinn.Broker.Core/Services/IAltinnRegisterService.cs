d21cf9a98938701c6baf4ad0ac686dc2923f7a4f
Magnus Åsheim <magnus.hansen.asheim@digdir.no>
2026-09-11 12:23:39 +0200

    register method to fetch org name

--- src/Altinn.Broker.Core/Services/IAltinnRegisterService.cs
+++ src/Altinn.Broker.Core/Services/IAltinnRegisterService.cs
@@ -3,4 +3,5 @@ public interface IAltinnRegisterService
 {
     Task<string?> LookUpOrganizationId(string organizationId, CancellationToken cancellationToken);
     Task<string?> LookupPartyByUuid(string partyUuid, CancellationToken cancellationToken);
+    Task<string?> LookupOrganizationName(string organizationId, CancellationToken cancellationToken);
 }
--- src/Altinn.Broker.Integrations/Altinn/Register/AltinnRegisterService.cs
+++ src/Altinn.Broker.Integrations/Altinn/Register/AltinnRegisterService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http.Json;
 using System.Text.RegularExpressions;
 
+using Altinn.Broker.Common;
 using Altinn.Broker.Core.Options;
 using Altinn.Broker.Core.Services;
 using Altinn.Platform.Register.Models;
@@ -62,5 +63,27 @@ public class AltinnRegisterService : IAltinnRegisterService
             return null;
         }
         return party.OrgNumber;
+    }
+
+    public async Task<string?> LookupOrganizationName(string organizationId, CancellationToken cancellationToken = default)
+    {
+        organizationId = organizationId.WithoutPrefix();
+        var partyLookup = new PartyLookup()
+        {
+            OrgNo = organizationId
+        };
+        var response = await _httpClient.PostAsJsonAsync("register/api/v1/parties/lookup", partyLookup, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            _logger.LogError("Error when looking up organization in Altinn Register.Statuscode was: {statusCode}, error was: {error}", response.StatusCode, await response.Content.ReadAsStringAsync());
+            return null;
+        }
+        var party = await response.Content.ReadFromJsonAsync<Party>();
+        if (party is null)
+        {
+            _logger.LogError("Unexpected json response when looking up organization in Altinn Register");
+            return null;
+        }
+        return party.Name.ToString();
     }
 }
